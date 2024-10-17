d8f8c4594d1f2b56a14df247823b6ee5e3377d76
Pedro Miotti <pedromiotti7@gmail.com>
2024-10-17 10:18:00 -0300

    Chore: Adjusted navbar for mobile

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import {
   Box,
   Flex,
@@ -11,12 +11,19 @@ import {
   Button,
   useBreakpointValue,
   Image,
-  Icon
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+  Icon,
+  Divider,
+  Hide,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { PiUserCircleFill } from "react-icons/pi";
 
-const Links = ['Porque a Kours?', 'Como funciona?', 'Feedback', 'Perguntas frequentes'];
+const Links = [
+  "Porque a Kours?",
+  "Como funciona?",
+  "Feedback",
+  "Perguntas frequentes",
+];
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -24,64 +31,102 @@ const Navbar = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      p="2rem"
-      w="full"
-    >
-      <Box bg="gray.600" color="white" px={4} borderRadius={8}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+    <Flex direction={"column"} alignItems="center" p="2rem" w="full">
+      <Box
+        bg="gray.600"
+        color="white"
+        px={4}
+        borderRadius={8}
+        w={{ base: "full", md: "auto" }}
+      >
+        <Flex
+          h={16}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          direction={{ base: "row-reverse", md: "row" }}
+          w={{ base: "full", md: "auto" }}
+        >
           <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ base: "flex", md: 'none' }}
+            size={"md"}
+            icon={
+              isOpen ? (
+                <CloseIcon color={"white"} fontSize={'12px'} />
+              ) : (
+                <HamburgerIcon color={"white"} fontSize={'20px'}/>
+              )
+            }
+            aria-label={"Open Menu"}
+            display={{ base: "flex", md: "none" }}
             onClick={isOpen ? onClose : onOpen}
+            variant={"unstyled"}
           />
-          <HStack spacing={8} alignItems={'center'} >
-            <Box><Image src='/white-logo.svg' alt='Logo' /></Box>
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <Image src="/white-logo.svg" alt="Logo" />
+            </Box>
             <HStack
-              as={'nav'}
+              as={"nav"}
               spacing={2}
               m="0 10rem"
-              fontSize={'sm'}
-              display={{ base: 'none', md: 'flex' }}>
+              fontSize={"sm"}
+              display={{ base: "none", md: "flex" }}
+            >
               {Links.map((link) => (
-                <Link key={link} px={4} py={3} rounded={'md'} _hover={{
-                  textDecoration: 'none',
-                  bg: 'gray.500',
-                  fontSize: 'sm'
-                }}>
+                <Link
+                  key={link}
+                  px={4}
+                  py={3}
+                  rounded={"md"}
+                  _hover={{
+                    textDecoration: "none",
+                    bg: "gray.500",
+                    fontSize: "sm",
+                  }}
+                >
                   {link}
                 </Link>
               ))}
             </HStack>
           </HStack>
-          <Flex alignItems={'center'} display={{ base: 'none', md: 'flex' }} borderLeft="1px solid #868686">
-            {/* <Button
+          <Hide below="md">
+            <Divider
+              orientation="vertical"
+              mr={3}
+              height={"50px"}
+              borderColor={"rgba(134, 134, 134, 0.3)"}
+            />
+          </Hide>
+          <Flex alignItems={"center"} display={{ base: "none", md: "flex" }}>
+            <Button
               leftIcon={<Icon as={PiUserCircleFill} boxSize={6} />}
               variant="solid"
               color="white"
               size="md"
               fontWeight={600}
-              _hover={{ bg: 'gray.500' }}
+              _hover={{ bg: "gray.500" }}
               bg="transparent"
               px={4}
+              disabled
             >
               Login
-            </Button> */}
+            </Button>
           </Flex>
         </Flex>
 
         {isMobile && isOpen ? (
-          <Box pb={4} display={{ md: 'none' }} w="full">
-            <Stack as={'nav'} spacing={4}>
+          <Box pb={4} display={{ md: "none" }} w="full">
+            <Stack as={"nav"} spacing={4}>
               {Links.map((link) => (
-                <Link key={link} px={3} py={2} rounded={'md'} _hover={{
-                  textDecoration: 'none',
-                  bg: 'teal.700',
-                }}>
+                <Link
+                  key={link}
+                  px={3}
+                  py={2}
+                  rounded={"md"}
+                  _hover={{
+                    textDecoration: "none",
+                    bg: "teal.700",
+                  }}
+                >
                   {link}
                 </Link>
               ))}
