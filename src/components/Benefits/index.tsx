3234983592f1a43e0fafc148e8f5012e0c04130b
Pedro Miotti <pedromiotti7@gmail.com>
2024-10-17 10:34:05 -0300

    Chore: Adjusted benefits

--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Flex, Box, Heading, Text, SimpleGrid, Icon } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  SimpleGrid,
+  Icon,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { BiSolidWidget } from "react-icons/bi";
 import { MdUpdate, MdDevices } from "react-icons/md";
 
@@ -18,29 +26,35 @@ const benefits = [
   {
     icon: MdDevices,
     title: "Acesso de qualquer lugar",
-    text: "Acesse a plataforma a partir de qualquer lugar, seja na versão web ou mobile, para total flexibilidade e conveniência.",
+    text: "Acesse a plataforma a partir de qualquer lugar, seja na versão web ou mobile, para total flexibilidade e conveniência.  Tornando assim o seu caminho para o sucesso digital o mais suave possível.",
   },
 ];
 
 export default function Features() {
+  const headingFontSize = useBreakpointValue({ base: "32px", md: "54px" });
+
   return (
     <Box bg="#F4F8FF">
-      <Box py={10} px={8} maxW="7xl" mx="auto">
-        <Box textAlign="center" fontWeight={600} my="4rem">
+      <Box py={8} px={8} maxW="7xl" mx="auto">
+        <Box
+          textAlign={{ base: "left", md: "center" }}
+          fontWeight={600}
+          my="4rem"
+        >
           <Text color="primary" fontSize="sm">
             Porque a Kours?
           </Text>
-          <Heading color="gray.500" fontSize="5xl">
+          <Heading color="gray.500" fontSize={headingFontSize} py={2}>
             Vantagens de fazer parte da Kours
           </Heading>
-          <Text color="gray.300" fontWeight={500}>
+          <Text color="gray.300" fontWeight={400}>
             Potencialize seu negócio digital.
           </Text>
         </Box>
         <SimpleGrid
           columns={{ base: 1, md: 3 }}
           spacing={10}
-          p="2rem"
+          p={{ base: '10px' ,md: "2rem" }}
           border="1px solid #e6e6e6"
           borderRadius="10px"
         >
@@ -49,13 +63,17 @@ export default function Features() {
               key={index}
               p={6}
               rounded="md"
-              boxShadow="xl"
-              _hover={{ bg: "white" }}
+              bg={"white"}
+              shadow={"md"}
               cursor="text"
             >
               <Flex direction="column" h="full">
                 <Icon as={benefits.icon} w={10} h={10} mb={4} color="primary" />
-                <Heading size="md" mb={2} color="gray.600">
+                <Heading
+                  fontSize={{ base: "16px", md: "20px" }}
+                  mb={2}
+                  color="gray.600"
+                >
                   {benefits.title}
                 </Heading>
                 <Text color="gray.500">{benefits.text}</Text>
