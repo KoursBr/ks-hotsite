3ee57ddd557116a4f6b9a2464995cf016e6abfbe
Pedro Miotti <pedromiotti7@gmail.com>
2024-11-17 21:05:05 -0300

    Feat: Added Google Tag Manager and Facebook Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
+
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,44 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <GoogleTagManager gtmId="G-WX6ZYHMRYF" />
+      <body className={inter.className}>
+        {children}
+
+        {/* Google Tag Manager  */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=G-WX6ZYHMRYF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1146058800520690');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=1146058800520690&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
